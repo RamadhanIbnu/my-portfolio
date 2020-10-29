e9f53b6221592c207e7d8d518cd905917bebaec6
ramadhan ibnu <ramadhanibnu77@gmail.com>
2020-10-29 18:39:08 +0700

    initial commit

---
+++ src/components/timeline.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Bootcamp Impact Byte <span>2020-present</span></h2>
+                        <p>I got experience working on structured projects with a team at this bootcamp, this place I also added my hard skills regarding front-end and back-end web development, not only that I also learned soft skills in this place, like expressing my opinion and get used to the interview.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Gunadarma University<span>2014-2020</span></h2>
+                        <p>I have finished my studies at Gunadarma University majoring in informatics engineering, there I got knowledge about programming fundamentals.</p>
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}

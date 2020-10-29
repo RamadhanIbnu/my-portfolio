1fc2c025045f949b7c3183d8e45d6bc6084e9d4d
ramadhan ibnu <ramadhanibnu77@gmail.com>
2020-10-29 21:02:14 +0700

    adding link to cv

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -15,7 +15,7 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
                           <h1>Hi! <br />I'm Ramadhan Ibnu</h1>
-                          <p><a className="btn btn-primary btn-learn" href="#"  rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1n6tcMzqFp7ts6HqqxynSmSntfSqFKk9y/view?usp=sharing" target="_blank"  rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>

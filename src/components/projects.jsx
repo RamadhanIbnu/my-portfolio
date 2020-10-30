9ff0aa79ed954019c6ec18d4d83a8a088df96519
ramadhan ibnu <ramadhanibnu77@gmail.com>
2020-10-30 13:43:22 +0700

    adding project

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -4,114 +4,303 @@ export default class Projects extends Component {
   render() {
     return (
       <div>
-				<section className="colorlib-work" data-section="projects">
-					<div className="colorlib-narrow-content">
-						<div className="row">
-							<div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-								<span className="heading-meta">My Work</span>
-								<h2 className="colorlib-heading animate-box">Recent Projects</h2>
-							</div>
-						</div>
-						<div className="row">
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
-								<div className="project" style={{backgroundImage: 'url(images/img-1.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">Work 01</a></h3>
-											<span>Website</span>
-											<p className="icon">
-												<span><a href="#"><i className="icon-share3" /></a></span>
-												<span><a href="#"><i className="icon-eye" /> 100</a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
-								<div className="project" style={{backgroundImage: 'url(images/img-2.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">Work 02</a></h3>
-											<span>Animation</span>
-											<p className="icon">
-												<span><a href="#"><i className="icon-share3" /></a></span>
-												<span><a href="#"><i className="icon-eye" /> 100</a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInTop">
-								<div className="project" style={{backgroundImage: 'url(images/img-3.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">Work 03</a></h3>
-											<span>Illustration</span>
-											<p className="icon">
-												<span><a href="#"><i className="icon-share3" /></a></span>
-												<span><a href="#"><i className="icon-eye" /> 100</a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInBottom">
-								<div className="project" style={{backgroundImage: 'url(images/img-4.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">Work 04</a></h3>
-											<span>Application</span>
-											<p className="icon">
-												<span><a href="#"><i className="icon-share3" /></a></span>
-												<span><a href="#"><i className="icon-eye" /> 100</a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
-								<div className="project" style={{backgroundImage: 'url(images/img-5.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">Work 05</a></h3>
-											<span>Graphic, Logo</span>
-											<p className="icon">
-												<span><a href="#"><i className="icon-share3" /></a></span>
-												<span><a href="#"><i className="icon-eye" /> 100</a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
-								<div className="project" style={{backgroundImage: 'url(images/img-6.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">Work 06</a></h3>
-											<span>Web Design</span>
-											<p className="icon">
-												<span><a href="#"><i className="icon-share3" /></a></span>
-												<span><a href="#"><i className="icon-eye" /> 100</a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div className="row">
-							<div className="col-md-12 animate-box">
-								<p><a href="#" className="btn btn-primary btn-lg btn-load-more">Load more <i className="icon-reload" /></a></p>
-							</div>
-						</div>
-					</div>
-				</section>
+        <section className="colorlib-work" data-section="projects">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div
+                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <span className="heading-meta">My Work</span>
+                <h2 className="colorlib-heading animate-box">
+                  Recent Projects
+                </h2>
+              </div>
+            </div>
+            <div className="row">
+              <div
+                className="col-md-4 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <div
+                  className="project"
+                  style={{ backgroundImage: "url(images/project-1.png)" }}
+                >
+                  <div className="desc">
+                    <div className="con">
+                      <h3>
+                        <a href="work.html">My Portfolio</a>
+                      </h3>
+                      <span>
+                        Website Portfolio about me and my project, i'm using
+                        React, and template from{" "}
+                        <a
+                          href="https://colorlib.com"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Colorlib
+                        </a>{" "}
+                        for this website
+                      </span>
+                      <p className="icon">
+                        <span>
+                          <a
+                            href="http://my-portfolio.ramadhanibnu.vercel.app/"
+                            target="_blank"
+                          >
+                            <i className="icon-link" />
+                            Link Web
+                          </a>
+                        </span>
+                        <span>
+                          <a href="images/project-1.png">
+                            <i className="icon-eye" target="_blank" /> see image
+                          </a>
+                        </span>
+                        <span>
+                          <a
+                            href="https://github.com/RamadhanIbnu/my-portfolio"
+                            target="_blank"
+                          >
+                            <i className="icon-code-outline" /> Source Code
+                          </a>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="col-md-4 animate-box"
+                data-animate-effect="fadeInRight"
+              >
+                <div
+                  className="project"
+                  style={{ backgroundImage: "url(images/project-2.png)" }}
+                >
+                  <div className="desc">
+                    <div className="con">
+                      <h3>
+                        <a href="work.html">Instancee</a>
+                      </h3>
+                      <span>
+                        This website has an educational theme, I work with my
+                        team to create this website using vanilla javascript,
+                        bootstrap, and jquery, I am in charge of the purchasing
+                        section such as shopping carts and payments, and several
+                        other functions
+                      </span>
+                      <p className="icon">
+                        <span>
+                          <a
+                            href="https://in5tancee.netlify.app/"
+                            target="_blank"
+                          >
+                            <i className="icon-link" />
+                            Link Web
+                          </a>
+                        </span>
+                        <span>
+                          <a href="images/project-2.png">
+                            <i className="icon-eye" target="_blank" /> see image
+                          </a>
+                        </span>
+                        <span>
+                          <a
+                            href="https://github.com/respatik/bimbel-online-in5tancee"
+                            target="_blank"
+                          >
+                            <i className="icon-code-outline" /> Source Code
+                          </a>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="col-md-4 animate-box"
+                data-animate-effect="fadeInTop"
+              >
+                <div
+                  className="project"
+                  style={{ backgroundImage: "url(images/project-3.png)" }}
+                >
+                  <div className="desc">
+                    <div className="con">
+                      <h3>
+                        <a href="work.html">Group-1-Flix</a>
+                      </h3>
+                      <span>
+                        This website has the theme of movie streaming, I with my
+                        team created this website using references from the
+                        <a href="https://goflix.se/" target="_blank" rel="noopener noreferrer"> GoFlix</a> web, and using vanilla javascript and vanilla
+                        css, I work in the Responsive Web Design section with
+                        media queries as a point break on a certain screen width
+                      </span>
+                      <p className="icon">
+                        <span>
+                          <a
+                            href="https://group-1-flix.netlify.app/"
+                            target="_blank"
+                          >
+                            <i className="icon-link" />
+                            Link Web
+                          </a>
+                        </span>
+                        <span>
+                          <a href="images/project-3.png">
+                            <i className="icon-eye" target="_blank" /> see image
+                          </a>
+                        </span>
+                        <span>
+                          <a
+                            href="https://github.com/mohammedazez/Group-1-Flix"
+                            target="_blank"
+                          >
+                            <i className="icon-code-outline" /> Source Code
+                          </a>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="col-md-4 animate-box"
+                data-animate-effect="fadeInBottom"
+              >
+                <div
+                  className="project"
+                  style={{ backgroundImage: "url(images/img-4.jpg)" }}
+                >
+                  <div className="desc">
+                    <div className="con">
+                      <h3>
+                        <a href="work.html">Project-4</a>
+                      </h3>
+                      <span>Comming soon</span>
+                      <p className="icon">
+                        <span>
+                          <a
+                            href="#"
+                          >
+                            <i className="icon-link" />
+                            Link Web
+                          </a>
+                        </span>
+                        <span>
+                          <a href="#">
+                            <i className="icon-eye" target="_blank" /> see image
+                          </a>
+                        </span>
+                        <span>
+                          <a
+                            href="#"
+                          >
+                            <i className="icon-code-outline" /> Source Code
+                          </a>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="col-md-4 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <div
+                  className="project"
+                  style={{ backgroundImage: "url(images/img-5.jpg)" }}
+                >
+                  <div className="desc">
+                    <div className="con">
+                      <h3>
+                        <a href="work.html">Project 5</a>
+                      </h3>
+                      <span>Comming Soon</span>
+                      <p className="icon">
+                        <span>
+                          <a
+                            href="#"
+                          >
+                            <i className="icon-link" />
+                            Link Web
+                          </a>
+                        </span>
+                        <span>
+                          <a href="#">
+                            <i className="icon-eye" target="_blank" /> see image
+                          </a>
+                        </span>
+                        <span>
+                          <a
+                            href="#"
+                          >
+                            <i className="icon-code-outline" /> Source Code
+                          </a>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="col-md-4 animate-box"
+                data-animate-effect="fadeInRight"
+              >
+                <div
+                  className="project"
+                  style={{ backgroundImage: "url(images/img-6.jpg)" }}
+                >
+                  <div className="desc">
+                    <div className="con">
+                      <h3>
+                        <a href="work.html">Project 6</a>
+                      </h3>
+                      <span>Comming Soon</span>
+                      <p className="icon">
+                        <span>
+                          <a
+                            href="#"
+                            
+                          >
+                            <i className="icon-link" />
+                            Link Web
+                          </a>
+                        </span>
+                        <span>
+                          <a href="#">
+                            <i className="icon-eye" target="_blank" /> see image
+                          </a>
+                        </span>
+                        <span>
+                          <a
+                            href="#"
+                          >
+                            <i className="icon-code-outline" /> Source Code
+                          </a>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12 animate-box">
+                <p>
+                  <a href="#" className="btn btn-primary btn-lg btn-load-more">
+                    Load more <i className="icon-reload" />
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
-    )
+    );
   }
 }

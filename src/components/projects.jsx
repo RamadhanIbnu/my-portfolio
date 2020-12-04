78768ce10c9e1a016f0e099aeb39bfc821318362
RamadhanIbnu <ramadhanibnu77@gmail.com>
2020-12-04 19:30:38 +0700

    adding modal for detail

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,123 +1,153 @@
-import React, {useState} from 'react'
-import {
-  slice, concat, 
-} from 'lodash';
+import React, { useState } from "react";
+import { slice, concat } from "lodash";
+// import { Modal, Button } from "react-bootstrap";
 
-const DATA = [ 
+const DATA = [
   {
-    name:"My Portfolio",
-    description:"Website Portfolio about me and my project, i'm using React, and template from colorlib",
-    image:"url(images/project-1.png)",
-    linkPorto:"http://my-portfolio.ramadhanibnu.vercel.app/",
-    linkImage:"images/project-1.png",
-    linkCode:"https://github.com/RamadhanIbnu/my-portfolio"
+    name: "My Portfolio",
+    description:
+      "Website Portfolio about me and my project, i'm using React, and template from colorlib",
+    image: "url(images/project-1.png)",
+    linkPorto: "http://my-portfolio.ramadhanibnu.vercel.app/",
+    linkImage: "images/project-1.png",
+    linkCode: "https://github.com/RamadhanIbnu/my-portfolio",
   },
   {
-    name:"Instancee",
-    description:"This website has an educational theme, I work with my team to create this website using vanilla javascript, bootstrap, and jquery, I am in charge of the purchasing section such as shopping carts and payments, and several other functions",
-    image:"url(images/project-2.png)",
-    linkPorto:"https://in5tancee.netlify.app/",
-    linkImage:"images/project-2.png",
-    linkCode:"https://github.com/respatik/bimbel-online-in5tancee"
+    name: "Instancee",
+    description:
+      "This website has an educational theme, I work with my team to create this website using vanilla javascript, bootstrap, and jquery, I am in charge of the purchasing section such as shopping carts and payments, and several other functions",
+    image: "url(images/project-2.png)",
+    linkPorto: "https://in5tancee.netlify.app/",
+    linkImage: "images/project-2.png",
+    linkCode: "https://github.com/respatik/bimbel-online-in5tancee",
   },
   {
-    name:"Group-1-Flix",
-    description:"This website has the theme of movie streaming, I with my team created this website using references from the GoFlix web, and using vanilla javascript and vanilla css, I work in the Responsive Web Design section with media queries as a point break on a certain screen width",
-    image:"url(images/project-3.png)",
-    linkPorto:"https://group-1-flix.netlify.app/",
-    linkImage:"images/project-3.png",
-    linkCode:"https://github.com/mohammedazez/Group-1-Flix"
+    name: "Group-1-Flix",
+    description:
+      "This website has the theme of movie streaming, I with my team created this website using references from the GoFlix web, and using vanilla javascript and vanilla css, I work in the Responsive Web Design section with media queries as a point break on a certain screen width",
+    image: "url(images/project-3.png)",
+    linkPorto: "https://group-1-flix.netlify.app/",
+    linkImage: "images/project-3.png",
+    linkCode: "https://github.com/mohammedazez/Group-1-Flix",
   },
   {
-    name:"Reuce",
-    description:"In this project, my team and I created a website that brings together recycling industry and waste owners to make it easier for waste owners to dispose of their trash that can still be recycled and at the same time campaigning about the importance of waste management. And in this project I work in Back-end area but I help Front-end to, like create Add Product section and fixing some bug",
-    image:"url(images/project-4.png)",
-    linkPorto:"https://reuce-web.netlify.app/",
-    linkImage:"images/project-4.png",
-    linkCode:"https://github.com/Supreme-Spyro/Reuce-Front"
-  }
- ];
+    name: "Reuce",
+    description:
+      "In this project, my team and I created a website that brings together recycling industry and waste owners to make it easier for waste owners to dispose of their trash that can still be recycled and at the same time campaigning about the importance of waste management. And in this project I work in Back-end area but I help Front-end to, like create Add Product section and fixing some bug",
+    image: "url(images/project-4.png)",
+    linkPorto: "https://reuce-web.netlify.app/",
+    linkImage: "images/project-4.png",
+    linkCode: "https://github.com/Supreme-Spyro/Reuce-Front",
+  },
+];
 // const IMAGE_SRC="https://source.unsplash.com/random";
-  const LIMIT = 3;
+const LIMIT = 3;
 
-const  Projects = () => {
+const Projects = () => {
+  const [showMore, setShowMore] = useState(true);
+  const [index, setIndex] = useState(LIMIT);
+  const [show, setShow] = useState(false);
+  const [list, setList] = useState(slice(DATA, 0, index));
+  console.log("data index: ", index);
 
-
-
-  const [showMore,setShowMore] = useState(true);
-  const [index,setIndex] = useState(LIMIT);
-  const [list,setList] = useState(slice(DATA, 0, index));
-  console.log("data index: ", index)
-
-  const loadMore = (e) =>{
-    e.preventDefault()
-    const newIndex = index + 3 ;
-    const newShowMore = newIndex < (DATA.length - 1);
+  const loadMore = (e) => {
+    e.preventDefault();
+    const newIndex = index + 3;
+    const newShowMore = newIndex < DATA.length - 1;
     // const newList =  slice(DATA, 0, newIndex);
     setIndex(newIndex);
     setList(slice(DATA, 0, newIndex));
     setShowMore(newShowMore);
-  }
+  };
 
-    return (
-      <div>
-        <section className="colorlib-work" data-section="projects">
-          <div className="colorlib-narrow-content">
-            <div className="row">
-              <div
-                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
-                data-animate-effect="fadeInLeft"
-              >
-                <span className="heading-meta">My Work</span>
-                <h2 className="colorlib-heading animate-box">
-                  Recent Projects
-                </h2>
-              </div>
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <div>
+      <section className="colorlib-work" data-section="projects">
+        <div className="colorlib-narrow-content">
+          <div className="row">
+            <div
+              className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+              data-animate-effect="fadeInLeft"
+            >
+              <span className="heading-meta">My Work</span>
+              <h2 className="colorlib-heading animate-box">Recent Projects</h2>
             </div>
-            <div className="row">
-              {console.log("data list: ", list)}
-              {list.map((item, index) => (
+          </div>
+          <div className="row">
+            {console.log("data list: ", list)}
+            {list.map((item, index) => (
+              <div
+                key={index}
+                className="col-md-4"
+                // data-animate-effect="fadeInLeft"
+              >
                 <div
-                  key={index}
-                  className="col-md-4"
-                  // data-animate-effect="fadeInLeft"
+                  className="project"
+                  style={{ backgroundImage: `${item.image}` }}
                 >
-                  <div
-                    className="project"
-                    style={{ backgroundImage: `${item.image}` }}
-                  >
-                    <div className="desc">
-                      <div className="con">
-                        <h3>
-                          <a href={item.linkPorto}>{item.name}</a>
+                  <div className="desc">
+                    <div className="con">
+                      <h3>
+                        <a href={item.linkPorto}>{item.name}</a>
+                      </h3>
+                      <span></span>
+                      <p className="icon">
+                        <span>
+                          <a href={item.linkPorto} target="_blank">
+                            <i className="icon-link" />
+                            Link
+                          </a>
+                        </span>
+                        <span>
+                          <a data-toggle="modal" data-target={`#${index}`}>
+                            <i className="icon-eye" target="_blank" /> Detail
+                          </a>
+                        </span>
+                        <span>
+                          <a href={item.linkCode} target="_blank">
+                            <i className="icon-code-outline" /> Code
+                          </a>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className="modal fade"
+                  id={index}
+                  tabindex="-1"
+                  aria-labelledby="exampleModalLabel"
+                  aria-hidden="true"
+                >
+                  <div className="modal-dialog">
+                    <div className="modal-content">
+                      <div className="modal-header">
+                        <h3 className="modal-title" id="exampleModalLabel" style={{textAlign:"center"}}>
+                          {item.name}
                         </h3>
-                        <span>{item.description}</span>
-                        <p className="icon">
-                          <span>
-                            <a href={item.linkPorto} target="_blank">
-                              <i className="icon-link" />
-                              Link
-                            </a>
-                          </span>
-                          <span>
-                            <a href={item.linkImage}>
-                              <i className="icon-eye" target="_blank" /> image
-                            </a>
-                          </span>
-                          <span>
-                            <a href={item.linkCode} target="_blank">
-                              <i className="icon-code-outline" /> Code
-                            </a>
-                          </span>
-                        </p>
+                      </div>
+                      <div className="modal-body">
+                        {item.description}
+                      </div>
+                      <div className="modal-footer">
+                        <button
+                          type="button"
+                          className="btn btn-secondary"
+                          data-dismiss="modal"
+                        >
+                          Close
+                        </button>
                       </div>
                     </div>
                   </div>
                 </div>
-              ))}
-              {showMore && <button onClick={loadMore}> Load More </button>}
-
-              {/* <div
+              </div>
+            ))}
+            {showMore && <button onClick={loadMore}> Load More </button>}
+            {/* <div
                 className="col-md-4 animate-box"
                 data-animate-effect="fadeInLeft"
               >
@@ -170,8 +200,8 @@ const  Projects = () => {
                   </div>
                 </div>
               </div> */}
-            </div>
-            {/* <div className="row">
+          </div>
+          {/* <div className="row">
               <div className="col-md-12 animate-box">
                 <p>
                   <a href="#" className="btn btn-primary btn-lg btn-load-more">
@@ -180,11 +210,10 @@ const  Projects = () => {
                 </p>
               </div>
             </div> */}
-          </div>
-        </section>
-      </div>
-    );
-  
-}
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default Projects;

d0846df88b46c03e8e44cd59c1543a050ee02f35
RamadhanIbnu <ramadhanibnu77@gmail.com>
2020-12-04 20:15:22 +0700

    edit loadmore and clean the code

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { slice, concat } from "lodash";
-// import { Modal, Button } from "react-bootstrap";
 
+// Data for Projects
 const DATA = [
   {
     name: "My Portfolio",
@@ -39,30 +39,54 @@ const DATA = [
     linkImage: "images/project-4.png",
     linkCode: "https://github.com/Supreme-Spyro/Reuce-Front",
   },
-  
+  {
+    name: "Group-1-Flix",
+    description:
+      "This website has the theme of movie streaming, I with my team created this website using references from the GoFlix web, and using vanilla javascript and vanilla css, I work in the Responsive Web Design section with media queries as a point break on a certain screen width",
+    image: "url(images/project-3.png)",
+    linkPorto: "https://group-1-flix.netlify.app/",
+    linkImage: "images/project-3.png",
+    linkCode: "https://github.com/mohammedazez/Group-1-Flix",
+  },
+  {
+    name: "Group-1-Flix",
+    description:
+      "This website has the theme of movie streaming, I with my team created this website using references from the GoFlix web, and using vanilla javascript and vanilla css, I work in the Responsive Web Design section with media queries as a point break on a certain screen width",
+    image: "url(images/project-3.png)",
+    linkPorto: "https://group-1-flix.netlify.app/",
+    linkImage: "images/project-3.png",
+    linkCode: "https://github.com/mohammedazez/Group-1-Flix",
+  },
+  {
+    name: "Group-1-Flix",
+    description:
+      "This website has the theme of movie streaming, I with my team created this website using references from the GoFlix web, and using vanilla javascript and vanilla css, I work in the Responsive Web Design section with media queries as a point break on a certain screen width",
+    image: "url(images/project-3.png)",
+    linkPorto: "https://group-1-flix.netlify.app/",
+    linkImage: "images/project-3.png",
+    linkCode: "https://github.com/mohammedazez/Group-1-Flix",
+  },
 ];
-// const IMAGE_SRC="https://source.unsplash.com/random";
 const LIMIT = 3;
 
 const Projects = () => {
+  // state for loadmore
   const [showMore, setShowMore] = useState(true);
+  // state for limit data, like how much you want to show your data
   const [index, setIndex] = useState(LIMIT);
-  const [show, setShow] = useState(false);
+  // state for listing DATA
   const [list, setList] = useState(slice(DATA, 0, index));
   console.log("data index: ", index);
 
+  // function for loadmore, when you click the loadmore then this function will give latest value for each state you have
   const loadMore = (e) => {
     e.preventDefault();
     const newIndex = index + 3;
     const newShowMore = index < DATA.length - 1;
-    // const newList =  slice(DATA, 0, newIndex);
     setIndex(newIndex);
     setList(slice(DATA, 0, newIndex));
     setShowMore(newShowMore);
   };
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
   return (
     <div>
@@ -80,11 +104,7 @@ const Projects = () => {
           <div className="row">
             {console.log("data list: ", list)}
             {list.map((item, index) => (
-              <div
-                key={index}
-                className="col-md-4"
-                // data-animate-effect="fadeInLeft"
-              >
+              <div key={index} className="col-md-4">
                 <div
                   className="project"
                   style={{ backgroundImage: `${item.image}` }}
@@ -126,13 +146,15 @@ const Projects = () => {
                   <div className="modal-dialog">
                     <div className="modal-content">
                       <div className="modal-header">
-                        <h3 className="modal-title" id="exampleModalLabel" style={{textAlign:"center"}}>
+                        <h3
+                          className="modal-title"
+                          id="exampleModalLabel"
+                          style={{ textAlign: "center" }}
+                        >
                           {item.name}
                         </h3>
                       </div>
-                      <div className="modal-body">
-                        {item.description}
-                      </div>
+                      <div className="modal-body">{item.description}</div>
                       <div className="modal-footer">
                         <button
                           type="button"
@@ -147,70 +169,21 @@ const Projects = () => {
                 </div>
               </div>
             ))}
-            {showMore && <button onClick={loadMore} style={{marginLeft:470}}> Load More </button>}
-            {/* <div
-                className="col-md-4 animate-box"
-                data-animate-effect="fadeInLeft"
-              >
-                <div
-                  className="project"
-                  style={{ backgroundImage: "url(images/project-1.png)" }}
-                >
-                  <div className="desc">
-                    <div className="con">
-                      <h3>
-                        <a href="work.html">My Portfolio</a>
-                      </h3>
-                      <span>
-                        Website Portfolio about me and my project, i'm using
-                        React, and template from{" "}
-                        <a
-                          href="https://colorlib.com"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          Colorlib
-                        </a>{" "}
-                        for this website
-                      </span>
-                      <p className="icon">
-                        <span>
-                          <a
-                            href="http://my-portfolio.ramadhanibnu.vercel.app/"
-                            target="_blank"
-                          >
-                            <i className="icon-link" />
-                            Link
-                          </a>
-                        </span>
-                        <span>
-                          <a href="images/project-1.png">
-                            <i className="icon-eye" target="_blank" /> image
-                          </a>
-                        </span>
-                        <span>
-                          <a
-                            href="https://github.com/RamadhanIbnu/my-portfolio"
-                            target="_blank"
-                          >
-                            <i className="icon-code-outline" /> Code
-                          </a>
-                        </span>
-                      </p>
-                    </div>
-                  </div>
+            {showMore && (
+              <div className="row">
+                <div className="col-md-12">
+                  <p>
+                    <a
+                      onClick={loadMore}
+                      className="btn btn-primary btn-lg btn-load-more"
+                    >
+                      Load more <i className="icon-reload" />
+                    </a>
+                  </p>
                 </div>
-              </div> */}
-          </div>
-          {/* <div className="row">
-              <div className="col-md-12 animate-box">
-                <p>
-                  <a href="#" className="btn btn-primary btn-lg btn-load-more">
-                    Load more <i className="icon-reload" />
-                  </a>
-                </p>
               </div>
-            </div> */}
+            )}
+          </div>
         </div>
       </section>
     </div>

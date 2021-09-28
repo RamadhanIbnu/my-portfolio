7e25dfcc81cbe1e5301bc8423e3e586b40eb4fd6
RamadhanIbnu <ramadhanibnu77@gmail.com>
2021-09-28 13:15:18 +0700

    edit linkedin link

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -28,7 +28,7 @@ export default class Sidebar extends Component {
                 <li><a href="https://www.facebook.com/ramadhan.ibnu.7" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
                 <li><a href="https://twitter.com/Ramadhan_IK" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
                 <li><a href="https://www.instagram.com/ramadhanibnu_/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
-                <li><a href="https://www.linkedin.com/in/ramadhan-ibnu-bb3241172/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
+                <li><a href="https://www.linkedin.com/in/ramadhan-ibnu-khatamsyah/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
                 <li><a href="https://github.com/RamadhanIbnu" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
               </ul>
             </nav>
